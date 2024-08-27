dcd301da1d59199d1c588d10039a9b4049348acc
GlobalTechInfo <tlptrends92@gmail.com>
2024-08-27 14:20:29 +0500

    𝙂𝙇𝙊𝘽𝘼𝙇-𝙓𝙈𝘿

---
+++ settings.js
@@ -0,0 +1,79 @@
+
+
+const fs = require('fs');
+const chalk = require('chalk');
+
+//owmner v card
+global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
+global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
+global.location = "Pakistan, Karachi" //ur location
+
+//new
+global.botname = 'GLOBAL-XMD' //ur bot name
+global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
+global.ownername = 'QasimAli' //ur owner name
+global.websitex = "https://youtu.be/WpfdClSeQyg"
+global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.themeemoji = '🪀'
+global.wm = "Global Bot Inc."
+global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-XMD' //script link
+global.packname = "Sticker By"
+global.author = "GlobalTechInfo\n\n+923444844060"
+global.creator = "923444844060@s.whatsapp.net"
+global.xprefix = '.'
+global.premium = ["923444844060"] // Premium User
+
+//channel id
+global.xchannel = {
+	jid: '120363222395675670@newsletter'
+	}
+
+//bot sett
+global.typemenu = 'v12' // menu type 'v1' => 'v12'
+global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.autoblocknumber = '91' //set autoblock country code
+global.antiforeignnumber = '91' //set anti foreign number country code
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json'
+
+global.limit = {
+	free: 100,
+	premium: 999,
+	vip: 'VIP'
+}
+
+global.uang = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	error: 'Error!',
+	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
+	done: 'Done'
+}
+
+global.bot = {
+	limit: 0,
+	uang: 0
+}
+
+global.game = {
+	suit: {},
+	menfes: {},
+	tictactoe: {},
+	kuismath: {},
+	tebakbom: {},
+}
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
