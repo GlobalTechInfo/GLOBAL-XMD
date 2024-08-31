19dfef433fcc79b6e5a8abb23b23f0a30f979d1a
Qasim Ali <166284738+GlobalTechInfo@users.noreply.github.com>
2024-08-31 19:06:09 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -34,7 +34,7 @@ global.typereply = 'v4' // reply type 'v1' => 'v4'
 global.autoblocknumber = '91' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 
-global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆','☆','⊡','益','✯','⊙','☞','㉨','✪','➜']
+global.listv = ['⫹⫺','•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆','☆','⊡','益','✯','⊙','☞','㉨','✪','➜']
 global.tempatDB = 'database.json'
 
 global.limit = {
